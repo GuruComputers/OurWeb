17bb2a6ad399218ec9db3d70497a08c618795610
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-11 20:28:30 +0000

    index.php skeleton file

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8">
-<title>Guru Computers</title>
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/main.css">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+	<title>Guru Computers</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
@@ -22,16 +22,22 @@
 
 <div id="container">
 	<div id="one">
-		<?php include '1.php' ?>
+		<?php include '1.php' ?>;
 	</div>
 	<div id="two">
-		<?php include '2.php' ?>
+		<?php include '2.php' ?>;
 	</div>
 	<div id="three">
-		<?php include '3.php' ?>
+		<?php include '3.php' ?>;
 	</div>
 	<div id="four">
-		<?php include '4.php' ?>
+		<?php include '4.php' ?>;
 	</div>
 </div>
 
+<div id="footer">
+	<?php include 'footer.php' ?>;
+</div>
+
+</body>
+</html>
