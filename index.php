fdba11dccad053e1f0e5de5b4e47c435ebf5fca4
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-11 20:12:56 +0000

    Add Skeleton Index

---
+++ index.php
@@ -0,0 +1,22 @@
+<div id="wrapper">
+	<div id="WebDev">
+		<?php
+			include '1.php';
+		?>
+	</div>
+	<div id="CompRepairs">
+		<?php
+			include '2.php';
+		?>
+	</div>
+	<div id="MobRepairs">
+		<?php
+			include '3.php';
+		?>
+	</div>
+	<div id="Shop">
+		<?php
+			include '4.php';
+		?>
+	</div>
+</div>
