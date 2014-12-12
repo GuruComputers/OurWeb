74e11b03a6d2933bd6cc3d9c7dd2244b4add899c
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-12 13:27:58 +0000

    facebook details

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 			<center>
 				<a href="mailto:support@gurucomputers.co.uk"><img src="./img/email.jpg" class="icons">Email Us</a>
 				<a href="https://www.youtube.com/user/gurucomputersuk"><img src="./img/youtube.png" class="icons">Guru's Channel</a>
-				<img src="./img/facebook.png" class="icons">
+				<a href="https://www.facebook.com/GuruComputersUK"><img src="./img/facebook.png" class="icons">Guru's Facebook</a>
 				<img src="./img/twitter.png" class="icons">
 				<br/>
 				<?php
