094ca9240ce63fe4f69b6f39f24f74fc6307616c
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 17:32:34 +0000

    Add Shop Summary

--- 4.php
+++ 4.php
@@ -1,10 +1,22 @@
 <center>
 	<h2 align="center"><a href="./shop/">Shop</a></h2><br />
 	<img src="img/shop.png" alt="Shop" title="Shop" />
+	<h3>Want an Upgrade?</h3>
+	<p>Since day zero, we have consistently developed our product range to provide the best choice, value, and technological opportunities for our customers. We have a wide and varied range of over 10,000 products lines from cables and laptop parts to servers and networking products.</p>
+	<p>We have direct relationships with some of the world’s leading manufacturers, and are UK distributors for a number of brands.</p>
+	<p>Ongoing development of our product range is very much influenced by our customers’ requirements, and we always aim to offer a selection of price / quality ranges within each area.</p>
+	<p>We have always retained our independence to source the best products at the best prices for our customers, have never been reliant on any one brand or area, and have never – and will never – take on any brand that is not consistent with our goals of adding value for our customers.</p>
+	<p>Our Product Range includes:-</p>
 	<ul>
-		<li>Motherboards</li>
-		<li>CPUs</li>
-		<li>Printing</li>
+		<li>Processors</li>
+		<li>CPU Fans & paste</li>
+		<li>Graphics Cards</li>
+		<li>Prebuilt PCs</li>
+		<li>Reconditioned PCs & Laptops</li>
+		<li>Networking Products</li>
+		<li>Peripherals</li>
+		<li>Cables</li>
+		<li>And Much Much More!</li>
 	</ul>
 	<div class="viewButton">
 		<a href="./shop">Shop Now</a>
