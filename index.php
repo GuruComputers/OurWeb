1d12c16db8f3a2dc3985fcba1e244a95b11de61a
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 14:08:43 +0000

    Footer Fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  			echo "<link rel=\"stylesheet\" href=\"./css/snow.css\">";
 		}
 	?>
-	<link href='http://fonts.googleapis.com/css?family=Poiret+One|Lobster|Indie+Flower|Raleway' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Poiret+One|Lobster|Indie+Flower|Raleway:400,500,600,700,800,900' rel='stylesheet' type='text/css'>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
