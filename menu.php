5a64fa2786e0a16f83f7da3313780639ceb0abf8
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-12 16:50:54 +0000

    switch round

--- menu.php
+++ menu.php
@@ -1,8 +1,8 @@
 <?php
-	if ($page != "home") {
-		$root = "../";
-	} else {
+	if ($page == "home") {
 		$root = "./";
+	} else {
+		$root = "../";
 	}
 	$menu = array("Home", "Portfolio", "Testimonials", "About", "Contact");
 	foreach ($menu as $item) {
