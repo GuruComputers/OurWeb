a91e06db82754d6eefeb94a74dfc5a4db538f281
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 18:10:52 +0000

    Add Mobile Phone Repair Summary

--- 3.php
+++ 3.php
@@ -2,11 +2,17 @@
 	<h2 align="center"><a href="./services/book.php">Mobile / Console Repairs</a></h2>
 	<img src="img/mobrepair.png" alt="Mobile / Console Repair" title="Mobile / Console Repair" />
 	<h3>Broken Screen?</h3>
-	<p>Here at Guru Computers Ltd, we specialize in Mobile Phone Repair.</p>
+	<p>Here at Guru Computers Ltd, we specialize in Electrical Repair, enabling us to repair most Phones, Tablets, Laptops and other Electrical devices.</p>
+	<p>Due to our unique blend of Hardware & Software Engineers, we are able to fix faults both Hardware & Software based including:-</p>
 	<ul>
 		<li>Screen Replacements</li>
 		<li>HDD Upgrades</li>
 		<li>Drive Replacements</li>
+		<li>Broken Casings</li>
+		<li>Battery or Charging Faults</li>
+		<li>Software Upgrades</li>
+		<li>Speaker / Microphone Faults</li>
+		<li>And Much Much More!</li>
 	</ul>
 	<div class="viewButton">
 		<a href="./services/book.php">Book a Repair</a>
--- 4.php
+++ 4.php
@@ -1,11 +1,10 @@
 <center>
 	<h2 align="center"><a href="./shop/">Shop</a></h2><br />
-	<img src="img/shop.png" alt="Shop" title="Shop" />
+	<img src="img/shop.png" alt="Shop" title="Shop" /><br style="line-height: 1.6em;" />
+
 	<h3>Want an Upgrade?</h3>
 	<p>Since day zero, we have consistently developed our product range to provide the best choice, value, and technological opportunities for our customers. We have a wide and varied range of over 10,000 products lines from cables and laptop parts to servers and networking products.</p>
 	<p>We have direct relationships with some of the world’s leading manufacturers, and are UK distributors for a number of brands.</p>
-	<p>Ongoing development of our product range is very much influenced by our customers’ requirements, and we always aim to offer a selection of price / quality ranges within each area.</p>
-	<p>We have always retained our independence to source the best products at the best prices for our customers, have never been reliant on any one brand or area, and have never – and will never – take on any brand that is not consistent with our goals of adding value for our customers.</p>
 	<p>Our Product Range includes:-</p>
 	<ul>
 		<li>Processors</li>
