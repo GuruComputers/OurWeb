2f3942267403d25df32285edf5d25eaef3d53335
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 10:22:28 +0000

    Add Images

--- 2.php
+++ 2.php
@@ -1,5 +1,6 @@
 <center>
 	<h3 align="center">Computer Repairs</h3>
+	<img src="img/comprepair.png" alt="Computer Repair" title="Computer Repair" />
 	<ul>
 		<li>Virus Removals</li>
 		<li>Upgrades</li>
