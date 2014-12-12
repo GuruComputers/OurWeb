09a8b321da944ca11620d2a392ed01b121c8768e
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 10:49:43 +0000

    Add Website Development Summary

--- 1.php
+++ 1.php
@@ -1,6 +1,9 @@
 <center>
 	<h3 align="center">Website Development</h3>
 	<img src="img/webdev.png" alt="Website Development" title="Website Development" />
+	<P>Need a new website? Our In-House Team handle all regular aspects of designing, hand-coding and hosting your website so you can relax knowing your Web Presence is in safe hands!</P>
+	<p>We are able to offer Virtual Private Servers from £10 a month, fully managed by our in-house engineers. We can also provide MySQL and Email Hosting within that price. This means you can have a single point of contact for your whole Web Presence, leaving you more time to run your business without the need to get invovled with the technical aspects of your Website.</p>
+	<p>Our Websites use the following Web Technologies:-</p>
 	<ul>
 		<li>HTML5</li>
 		<li>CSS3</li>
