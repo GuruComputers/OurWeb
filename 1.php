0206c60223bcdad2cf610280be0dece165951c03
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 15:00:45 +0000

    Add Computer Repairs Summary

--- 1.php
+++ 1.php
@@ -3,7 +3,7 @@
 	<img src="img/webdev.png" alt="Website Development" title="Website Development" />
 	<h3>Need a new website? </h3>
 	<p>Our In-House Team handle all regular aspects of designing, hand-coding and hosting your website so you can relax knowing your Web Presence is in safe hands!</P>
-	<p>We are able to offer Virtual Private Servers from £10 a month, fully managed by our in-house engineers. We can also provide MySQL and Email Hosting within that price. This means you can have a single point of contact for your whole Web Presence, leaving you more time to run your business without the need to get invovled with the technical aspects of your Website.</p>
+	<p>We are able to offer Virtual Private Servers from £10 a month, fully managed by our in-house engineers. We can also provide MySQL and Email Hosting within that price. This means you can have a single point of contact for your whole Web Presence, leaving you more time to run your business without the need to get involved with the technical aspects of your Website.</p>
 	<p>Our Websites use the following Web Technologies:-</p>
 	<ul>
 		<li>HTML5</li>
--- 2.php
+++ 2.php
@@ -1,10 +1,17 @@
 <center>
 	<h2 align="center"><a href="./services/book.php">Computer<br />Repairs</a></h2>
 	<img src="img/comprepair.png" alt="Computer Repair" title="Computer Repair" />
+	<h3>Computer Running Slow?</h3>
+	<p>Here at Guru Computers Ltd we nevr put a healthy computer down!</p>
+	<p>We have been busting Computer Viruses as a team for the best part of 17 years, and we aren't going to stop now! With the use of Multi-layered Antivirus Technology, we can provide fast and effective protection against current and future threats.</p>
+	<p>The bottom line is we keep you safe and we keep it simple! Our range of services include:- </p>
 	<ul>
 		<li>Virus Removals</li>
-		<li>Upgrades</li>
+		<li>PC Upgrades</li>
 		<li>Reinstalls</li>
+		<li>Laptop Screen Replacements</li>
+		<li>Remote Support</li>
+		<li>Data Recovery</li>
 	</ul>
 	<a href="./services/book.php" class="viewButton"><p>Book a Repair</p></a>
 </center>
