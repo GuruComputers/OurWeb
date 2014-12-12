047e632d073e1998be2522fc07dde9b49df99e83
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-12 16:47:28 +0000

    small is best

--- menu.php
+++ menu.php
@@ -1,8 +1,13 @@
 <?php
+	if ($page != "home") {
+		$root = "../";
+	} else {
+		$root = "./";
+	}
 	$menu = array("Home", "Portfolio", "Testimonials", "About", "Contact");
 	foreach ($menu as $item) {
 		if (strtolower($item) != "home"){
-			echo '<li><a href="'.'./'.strtolower($item).'/"';
+			echo '<li><a href="'.$root.strtolower($item).'/"';
 		} else {
 			echo '<li><a href="./"';
 		}
