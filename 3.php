7a04c9c31ae31b1644d58cc83d506fb6daf641db
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 16:28:33 +0000

    Remove Test Snippet

--- 3.php
+++ 3.php
@@ -2,7 +2,7 @@
 	<h2 align="center"><a href="./services/book.php">Mobile / Console Repairs</a></h2>
 	<img src="img/mobrepair.png" alt="Mobile / Console Repair" title="Mobile / Console Repair" />
 	<h3>Broken Screen?</h3>
-	<p class="selected">Here at Guru Computers Ltd, we specialize in Mobile Phone Repair.</p>
+	<p>Here at Guru Computers Ltd, we specialize in Mobile Phone Repair.</p>
 	<ul>
 		<li>Screen Replacements</li>
 		<li>HDD Upgrades</li>
