85b66169d0238234da6f0862b534d0beb699f1d9
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 16:17:04 +0000

    Skeleton Mobile Repair Text

--- 2.php
+++ 2.php
@@ -2,7 +2,7 @@
 	<h2 align="center"><a href="./services/book.php">Computer<br />Repairs</a></h2>
 	<img src="img/comprepair.png" alt="Computer Repair" title="Computer Repair" />
 	<h3>Computer Running Slow?</h3>
-	<p>Here at Guru Computers Ltd we nevr put a healthy computer down!</p>
+	<p>Here at Guru Computers Ltd we never put a healthy computer down!</p>
 	<p>We have been busting Computer Viruses as a team for the best part of 17 years, and we aren't going to stop now! With the use of Multi-layered Antivirus Technology, we can provide fast and effective protection against current and future threats.</p>
 	<p>The bottom line is we keep you safe and we keep it simple! Our range of services include:- </p>
 	<ul>
--- 3.php
+++ 3.php
@@ -1,6 +1,8 @@
 <center>
 	<h2 align="center"><a href="./services/book.php">Mobile / Console Repairs</a></h2>
 	<img src="img/mobrepair.png" alt="Mobile / Console Repair" title="Mobile / Console Repair" />
+	<h3>Broken Screen?</h3>
+	<p class="selected">Here at Guru Computers Ltd, we specialize in Mobile Phone Repair.</p>
 	<ul>
 		<li>Screen Replacements</li>
 		<li>HDD Upgrades</li>
