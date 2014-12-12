d7d9a448d5988315913d77450ff84e2961e63634
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-12 13:32:32 +0000

    Add Snow

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 	<title>Guru Computers</title>
 	<link rel="stylesheet" href="./css/normalize.css">
 	<link rel="stylesheet" href="./css/main.css">
+	<?php
+		if (date('m') == '12') {
+ 			echo "<link rel=\"stylesheet\" href=\"./css/snow.css\">";
+		}
+	?>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
