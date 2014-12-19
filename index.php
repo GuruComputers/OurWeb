ea1e84c2636bbb8613c8eaa76e349d986fac353c
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-19 18:03:05 +0000

    removed smroche img

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
  			echo '<link rel="stylesheet" href="'.$root.'css/snow.css">'."\n";
 		}
 	?>
+	<meta name="description" content="Guru Computers Ltd pride ouselves on excellent customer service. we go the extra mile so you don't have too. Our services range from computer parts to bespoke website design. Need a demo cd designed, cd's burnt, printed, packaged then look no further we can do it.">
 	<link href='http://fonts.googleapis.com/css?family=Poiret+One|Lobster|Indie+Flower|Raleway:400,500,600,700,800,900' rel='stylesheet' type='text/css'>
 </head>
 
