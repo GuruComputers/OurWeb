ef04c6fd60c56c0f1aab70baf2b284f31d1ab5a5
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-27 16:04:17 +0000

    Adjust Printing Services Text

--- 4.php
+++ 4.php
@@ -3,7 +3,7 @@
 	<img src="img/print.png" alt="Print" title="A brief Intro to our Printing Services" /><br style="line-height: 1.6em;" />
 
 	<h3>Whatever your Needs, We've got your covered!</h3>
-	<p>Since day zero, we have consistently developed our product range to provide the best choice, value, and technological opportunities for our customers. We have a wide and varied range of over 10,000 products lines from cables and laptop parts to servers and networking products.</p>
+	<p>Since day zero, we have consistently developed our Printed Product range to provide the best choice, value, and technological opportunities for our customers. We have a wide and varied range of over 100 media types from CD and DVD to Flyers and Promotional Products.</p>
 	<p>We have direct relationships with some of the world’s leading manufacturers, and are UK distributors for a number of brands.</p>
 	<p>Our Printing Range includes:-</p>
 	<ul>
