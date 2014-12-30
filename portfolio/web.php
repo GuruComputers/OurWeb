e5a256cc4b13f01fe978113d1680576cf83cbec6
Rev Neil Wakely <neil@gurucomputers.co.uk>
2014-12-30 20:58:10 +0000

    updated web.php to new format

--- portfolio/web.php
+++ portfolio/web.php
@@ -8,18 +8,9 @@
 <body>
 	<div id="wrapper">
 		<div id="top">
-			<header>
-				<?php
-					echo '<a href="'.$root.'" id="logo"><img src="'.$root.'img/logo.png" class="logo"></a>'."\n";
-				?>
-				<nav>
-					<ul>
-						<?php 
-							include $root.'menu.php';
-						?>
-					</ul>
-				</nav>
-			</header>
+			<?php
+				include $root."includes/header.php";
+			?>
 		</div> <!-- top div closed -->
 		<?php
 			echo '<img src="'.$root.'img/xmashr.png" class="hr">';
