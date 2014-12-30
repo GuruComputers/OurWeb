38bd5734ab00e4e3f6cbef4a62adb374139f236a
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-30 19:51:05 +0000

    Includes Init

---
+++ includes/menu.php
@@ -0,0 +1,14 @@
+<?php
+	$menu = array("Home", "Portfolio", "Testimonials", "Services", "Shop", "About", "Contact");
+	foreach ($menu as $item) {
+		if (strtolower($item) != "home"){
+			echo '<li><a href="'.$root.strtolower($item).'/"';
+		} else {
+			echo '<li><a href="'.$root.'"';
+		}
+   	 	if (strtolower($item) == $page){
+   	 		echo ' class="selected"';
+   	 	}
+   	 	echo '>'.$item.'</a></li>'."\n";
+   	 }
+?>
