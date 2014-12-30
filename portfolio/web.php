08ab5f35aee8341faa707b43cd2a3eb78463ec6a
Rev Dave Wakely <dave@gurucomputers.co.uk>
2014-12-30 20:51:35 +0000

    Finished Web Portfolio Page
    (I Think)

--- portfolio/web.php
+++ portfolio/web.php
@@ -1,23 +1,9 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<title>Guru Computers</title>
 	<?php
-		$page = "portfolio";
-		if ($page == "home") {
-			$root = "./";
-		} else {
-			$root = "../";
-		}
-		echo '<link rel="stylesheet" href="'.$root.'css/normalize.css">'."\n";
-		echo '<link rel="stylesheet" href="'.$root.'css/main.css">'."\n";
-		if (date('m') == '12') {
- 			echo '<link rel="stylesheet" href="'.$root.'css/snow.css">'."\n";
-		}
+		$page = "porfolio";
+		include "./includes/head.php";
 	?>
-	<link href='http://fonts.googleapis.com/css?family=Poiret+One|Lobster|Indie+Flower|Raleway:400,500,600,700,800,900' rel='stylesheet' type='text/css'>
-</head>
 
 <body>
 	<div id="wrapper">
