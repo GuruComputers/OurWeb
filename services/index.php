965f5d5c000d40b527df4d56cfecfb0489dbd479
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-10 19:40:36 +0000

    Add Service Prices

--- services/index.php
+++ services/index.php
@@ -6,7 +6,69 @@
 		<div id="content">
 			<center>
 				<h1>Our Services</h1>
-				
+				<p>To book a computer service, click <a href="./book/">here</a></p>
+				<p>Alternatively, give us a call on <a href="tel:01273253567">(01273) 253567</a></p>
+				<div class="ServicesTable">
+                <table >
+                    <tr>
+                        <td>
+                            Service
+                        </td>
+                        <td >
+                            Cost
+                        </td>
+                    </tr>
+                    <tr>
+                        <td >
+                            CD Printing & Duplication
+                        </td>
+                        <td>
+                            £0.75p per Unit
+                        </td>
+                    </tr>
+                    <tr>
+                        <td >
+                            Computer General Service
+                        </td>
+                        <td>
+                            £30
+                        </td>
+                    </tr>
+                    <tr>
+                        <td >
+                            Operating System Reinstall
+                        </td>
+                        <td>
+                            £30
+                        </td>
+                    </tr>
+                    <tr>
+                        <td >
+                            iPhone Screen Replacement
+                        </td>
+                        <td>
+                            £40
+                        </td>
+                    </tr>
+                    <tr>
+                        <td >
+                            Website Hosting (Unmanaged)
+                        </td>
+                        <td>
+                            £10 a month
+                        </td>
+                    </tr>
+                    <tr>
+                        <td >
+                            Website Hosting (Managed)
+                        </td>
+                        <td>
+                            £15 a month
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            
 			</center>	
 		</div><!-- Content Div Closed -->
 		
